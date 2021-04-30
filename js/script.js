cdfa6bc8368b8ee66accdfae8963fba77598b5bf
strayder <vyacheslav.jaworski@gmail.com>
2021-04-30 17:20:16 +0300

    second

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function (){
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('look');
     })
+    $('.slider_reveiw').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
+    $('.slider_mentors').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 10000
+    });
 });
+
+// slider
